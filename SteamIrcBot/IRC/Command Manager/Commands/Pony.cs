bf5fd5ecadd366c4a01ec0db154b2678bca23bc6
Ryan Stecker <voidedweasel@gmail.com>
2013-02-16 00:56:25 -0600

    Implement !pony command.

---
+++ SteamIrcBot/IRC/Command Manager/Commands/Pony.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace SteamIrcBot
+{
+    class PonyCommand : Command<PonyCommand.Request>
+    {
+        public class Request : BaseRequest
+        {
+        }
+
+        public PonyCommand()
+        {
+            Trigger = "!pony";
+            HelpText = "!pony - pony verb the noun";
+        }
+
+        protected override void OnRun( CommandDetails details )
+        {
+            using ( var webClient = new WebClient() )
+            {
+                var req = new Request();
+                AddRequest( details, req );
+
+                webClient.DownloadStringCompleted += OnDownloadCompleted;
+                webClient.DownloadStringAsync( new Uri( "http://areweponyyet.com/?chatty=1" ), req );
+            }
+        }
+
+        void OnDownloadCompleted( object sender, DownloadStringCompletedEventArgs e )
+        {
+            var req = GetRequest( r => e.UserState == r );
+
+            if ( req == null )
+                return;
+
+            if ( e.Error != null )
+            {
+                IRC.Instance.Send( req.Channel, "{0}: Unable to request a pony video: {1}", req.Requester.Nickname, e.Error.Message );
+                return;
+            }
+
+            IRC.Instance.Send( req.Channel, "{0}: http://areweponyyet.com/{1}", req.Requester.Nickname, e.Result );
+        }
+    }
+}
