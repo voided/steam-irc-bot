2737341f6a440f9648bc4d4a97328dd6f35202aa
Ryan Stecker <voidedweasel@gmail.com>
2013-05-29 00:19:08 -0500

    Correctly handle the TF2 GC's goodbye message.

--- SteamIrcBot/Steam/GC Manager/GC Handlers/GCHandlers.cs
+++ SteamIrcBot/Steam/GC Manager/GC Handlers/GCHandlers.cs
@@ -14,13 +14,18 @@ namespace SteamIrcBot
             : base( manager )
         {
             new GCCallback<CMsgClientWelcome>( ( uint )EGCBaseClientMsg.k_EMsgGCClientWelcome, OnWelcome, manager );
+
+            // these two callbacks exist to cover the gc message differences between dota and tf2
+            // in TF2, it still uses k_EMsgGCClientGoodbye, whereas dota is using k_EMsgGCClientConnectionStatus
+            // luckily the message format between the two messages is the same
             new GCCallback<CMsgConnectionStatus>( ( uint )EGCBaseClientMsg.k_EMsgGCClientConnectionStatus, OnConnectionStatus, manager );
+            new GCCallback<CMsgConnectionStatus>( 4008 /* tf2's k_EMsgGCClientGoodbye */, OnConnectionStatus, manager );
         }
 
 
         void OnWelcome( ClientGCMsgProtobuf<CMsgClientWelcome> msg )
         {
-            IRC.Instance.SendAll( "New {0} GC session (version: {1})", Settings.Current.GCApp, msg.Body.version );
+            IRC.Instance.SendAll( "New {0} GC session (version: {1})", Steam.Instance.GetAppName( Settings.Current.GCApp ), msg.Body.version );
         }
 
         void OnConnectionStatus( ClientGCMsgProtobuf<CMsgConnectionStatus> msg )
@@ -62,9 +67,7 @@ namespace SteamIrcBot
             {
                 lastSchemaVersion = msg.Body.item_schema_version;
 
-#if SERVICE_BUILD // don't be so spammy in dev builds
                 IRC.Instance.SendAll( "New GC item schema (version: " + lastSchemaVersion.ToString( "X4" ) + "): " + msg.Body.items_game_url );
-#endif
             }
         }
     }
