578eaf1cf1582abce262ecab763c94e8b8018bf8
Ryan Stecker <voidedweasel@gmail.com>
2013-05-30 16:11:34 -0500

    Include clan name in announcement and event messages.

--- SteamIrcBot/Steam/Steam Manager/Handlers/GroupHandler.cs
+++ SteamIrcBot/Steam/Steam Manager/Handlers/GroupHandler.cs
@@ -20,13 +20,13 @@ namespace SteamIrcBot
             foreach ( var announcement in callback.Announcements )
             {
                 string announceUrl = string.Format( "http://steamcommunity.com/gid/{0}/announcements/detail/{1}", callback.ClanID.ConvertToUInt64(), announcement.ID.Value );
-                IRC.Instance.SendAll( "Group announcement: {0} - {1}", announcement.Headline, announceUrl );
+                IRC.Instance.SendAll( "{0} announcement: {1} - {2}", callback.ClanName, announcement.Headline, announceUrl );
             }
 
             foreach ( var clanEvent in callback.Events )
             {
                 string eventUrl = string.Format( "http://steamcommunity.com/gid/{0}/events/{1}", callback.ClanID.ConvertToUInt64(), clanEvent.ID.Value );
-                IRC.Instance.SendAll( "Group event: {0} - {1}", clanEvent.Headline, eventUrl );
+                IRC.Instance.SendAll( "{0} event: {1} - {2}", callback.ClanName, clanEvent.Headline, eventUrl );
             }
         }
     }
