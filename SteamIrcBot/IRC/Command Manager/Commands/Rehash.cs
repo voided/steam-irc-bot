75b8b950eeb0a82c2a6a3f23f2bc431941627542
Ryan Stecker <voidedweasel@gmail.com>
2013-06-03 01:21:27 -0500

    Add rehash command to reload settings.

---
+++ SteamIrcBot/IRC/Command Manager/Commands/Rehash.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SteamIrcBot
+{
+    class RehashCommand : Command
+    {
+        DateTime lastRehash = DateTime.Now;
+
+        public RehashCommand()
+        {
+            Trigger = "!rehash";
+            HelpText = "!rehash - Reloads bot settings";
+        }
+
+        protected override void OnRun( CommandDetails details )
+        {
+            TimeSpan timeDiff = DateTime.Now - lastRehash;
+
+            if ( timeDiff <= TimeSpan.FromSeconds( 5 ) )
+            {
+                IRC.Instance.Send( details.Channel, "{0}: Cannot rehash yet", details.Sender.Nickname );
+                return;
+            }
+
+            lastRehash = DateTime.Now;
+
+            try
+            {
+                Settings.Load();
+            }
+            catch ( Exception ex )
+            {
+                IRC.Instance.Send( details.Channel, "{0}: Unable to load settings. Error has been logged", details.Sender.Nickname );
+                Log.WriteError( "RehashCommand", "Unable to rehash settings: {0}", ex.Message );
+                return;
+            }
+
+            if ( !Settings.Validate() )
+            {
+                IRC.Instance.Send( details.Channel, "{0}: Settings did not validate. Error has been logged", details.Sender.Nickname );
+                return;
+            }
+
+            IRC.Instance.Send( details.Channel, "{0}: Rehashed", details.Sender.Nickname );
+        }
+    }
+}
