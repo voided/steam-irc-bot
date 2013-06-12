9be20f2b49343d52f0e53fb3199aab1f37e5d13c
Ryan Stecker <voidedweasel@gmail.com>
2013-06-11 19:00:46 -0500

    Add UpToDateJob which monitors update state of important flagged apps.

---
+++ SteamIrcBot/Steam/Job Manager/Jobs/UpToDateJob.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Net;
+using SteamKit2;
+
+namespace SteamIrcBot
+{
+    class UpToDateJob : Job
+    {
+        ConcurrentDictionary<uint, uint> versionMap;
+
+
+        public UpToDateJob( CallbackManager manager )
+        {
+            Period = TimeSpan.FromMinutes( 1 );
+
+            versionMap = new ConcurrentDictionary<uint, uint>();
+        }
+
+        protected override void OnRun()
+        {
+            Parallel.ForEach( Settings.Current.ImportantApps, app =>
+            {
+                uint lastVersion = 0;
+                bool hasLastVersion = versionMap.TryGetValue( app, out lastVersion );
+
+                using ( dynamic steamApps = WebAPI.GetInterface( "ISteamApps" ) )
+                {
+                    steamApps.Timeout = ( int )TimeSpan.FromSeconds( 5 ).TotalMilliseconds;
+
+                    KeyValue results = null;
+
+                    try
+                    {
+                        results = steamApps.UpToDateCheck( appid: app, version: lastVersion );
+                    }
+                    catch ( WebException ex )
+                    {
+                        Log.WriteWarn( "UpToDateJob", "Unable to make UpToDateCheck request: {0}", ex.Message );
+                        return;
+                    }
+
+                    if ( !results[ "success" ].AsBoolean() )
+                        return; // no useful result from the api, or app isn't configured
+
+                    uint requiredVersion = ( uint )results[ "required_version" ].AsInteger();
+
+                    if ( !results[ "up_to_date" ].AsBoolean() )
+                    {
+                        // update our cache of required version
+                        versionMap[ app ] = requiredVersion;
+
+                        if ( hasLastVersion )
+                        {
+                            // if we previously cached the version, display that it changed
+                            IRC.Instance.SendAnnounce( "{0} (version: {1}) is no longer up to date. New version: {2}", Steam.Instance.GetAppName( app ), lastVersion, requiredVersion );
+                        }
+                    }
+
+                }
+            } );
+        }
+    }
+}
