bd4ae8cdb51b9cef91ac7fa9e16a54aa41fa5fb7
Ryan Stecker <voidedweasel@gmail.com>
2013-07-27 19:44:21 -0500

    Commit some missing changes from TPL Task change.

---
+++ SteamIrcBot/Steam/Task Manager/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SteamKit2;
+
+namespace SteamIrcBot
+{
+    abstract class BaseTask
+    {
+        public abstract void Handle( object callback );
+    }
+
+    class JobTask<T> : BaseTask
+        where T : CallbackMsg
+    {
+        public Task<T> Task { get { return completionSource.Task; } }
+
+
+        TaskCompletionSource<T> completionSource = new TaskCompletionSource<T>();
+
+
+        public override void Handle( object callback )
+        {
+            SteamClient.JobCallback<T> jobCallback = callback as SteamClient.JobCallback<T>;
+
+            completionSource.SetResult( jobCallback.Callback );
+        }
+    }
+}
