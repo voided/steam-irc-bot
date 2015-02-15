dba42d52c3a32ba15f8aa5c49ac50b57ac05e78d
Ryan Stecker <ryan@stecker.email>
2015-02-14 18:25:05 -0600

    Refactor out PluralizeFormatProvider into utils.

--- SteamIrcBot/Utils/Utils.cs
+++ SteamIrcBot/Utils/Utils.cs
@@ -285,4 +285,48 @@ namespace SteamIrcBot
         }
     }
 
+    class PluralizeFormatProvider : IFormatProvider, ICustomFormatter
+    {
+        /// <summary>
+        /// Returns an object that provides formatting services for the specified type.
+        /// </summary>
+        /// <param name="formatType">An object that specifies the type of format object to return.</param>
+        /// <returns>
+        /// An instance of the object specified by <paramref name="formatType" />, if the <see cref="T:System.IFormatProvider" /> implementation can supply that type of object; otherwise, null.
+        /// </returns>
+        public object GetFormat( Type formatType )
+        {
+            return this;
+        }
+
+        /// <summary>
+        /// Converts the value of a specified object to an equivalent string representation using specified format and culture-specific formatting information.
+        /// </summary>
+        /// <param name="format">A format string containing formatting specifications.</param>
+        /// <param name="arg">An object to format.</param>
+        /// <param name="formatProvider">An object that supplies format information about the current instance.</param>
+        /// <returns>
+        /// The string representation of the value of <paramref name="arg" />, formatted as specified by <paramref name="format" /> and <paramref name="formatProvider" />.
+        /// </returns>
+        public string Format( string format, object arg, IFormatProvider formatProvider )
+        {
+            if ( format == null )
+                return arg.ToString();
+
+            string[] forms = format.Split( new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries );
+
+            if ( arg is int )
+            {
+                int value = (int)arg;
+
+                if ( value == 1 )
+                    return string.Format( "{0} {1}", value, forms[ 0 ] );
+
+                return string.Format( "{0} {1}", value, forms[ 1 ] );
+            }
+
+            return arg.ToString();
+        }
+    }
+
 }
