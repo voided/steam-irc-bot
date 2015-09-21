0a4687dcdc45f1ef5c2ed4fb5e6d3c1204fddc9c
Ryan Stecker <ryan@stecker.email>
2015-09-21 10:58:36 -0500

    Add !topcustoms command.

---
+++ SteamIrcBot/IRC/Commands/Dota/TopCustoms.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamIrcBot
+{
+    class TopCustomsCommand : Command<TopCustomsCommand.Request>
+    {
+        public class Request : BaseRequest
+        {
+        }
+
+        public TopCustomsCommand()
+        {
+            Triggers.Add( "!topcustoms" );
+
+            HelpText = "!topcustoms - Show the top custom dota games";
+        }
+
+        protected override void OnRun( CommandDetails details )
+        {
+            string displayString = DotaGCTopGamesHandler.Instance.GetDisplay();
+
+            IRC.Instance.Send( details.Channel, "{0}: {1}", details.Sender.Nickname, displayString );
+        }
+    }
+}
