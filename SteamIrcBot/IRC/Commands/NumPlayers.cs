c8455490fb244f1e1f10dd890283a28ca1caf2ba
Ryan Stecker <ryan@stecker.email>
2016-01-29 18:09:29 -0600

    Update !numplayers to use latest SteamKit changes.

--- SteamIrcBot/IRC/Commands/NumPlayers.cs
+++ SteamIrcBot/IRC/Commands/NumPlayers.cs
@@ -12,7 +12,7 @@ namespace SteamIrcBot
         {
             public JobID JobID { get; set; }
 
-            public GameID GameID { get; set; }
+            public uint AppID { get; set; }
         }
 
 
@@ -22,7 +22,7 @@ namespace SteamIrcBot
 
             Triggers.Add( "!numplayers" );
             Triggers.Add( "!players" );
-            HelpText = "!numplayers <gameid/name> - Requests the current number of players playing the given GameID or app name, according to Steam";
+            HelpText = "!numplayers <appid/name> - Requests the current number of players playing the given GameID or app name, according to Steam";
         }
 
 
@@ -30,48 +30,43 @@ namespace SteamIrcBot
         {
             if ( details.Args.Length == 0 )
             {
-                IRC.Instance.Send( details.Channel, "{0}: GameID argument required", details.Sender.Nickname );
+                IRC.Instance.Send( details.Channel, "{0}: AppID argument required", details.Sender.Nickname );
                 return;
             }
             
-            ulong gameId;
-            if ( !ulong.TryParse( details.Args[ 0 ], out gameId ) )
+            uint appId;
+            if ( !uint.TryParse( details.Args[ 0 ], out appId ) )
             {
                 // lets search by name if we're not a gameid
 
                 string appName = string.Join( " ", details.Args );
 
-                uint appId;
                 if ( !Steam.Instance.AppInfo.FindApp( appName, out appId, gamesOnly: true ) )
                 {
                     IRC.Instance.Send( details.Channel, "{0}: Invalid GameID or unknown app name", details.Sender.Nickname );
                     return;
                 }
-
-                gameId = appId;
             }
-
-            GameID realGameID = gameId;
 
             if ( !Steam.Instance.Connected )
             {
-                IRC.Instance.Send( details.Channel, "{0}: Unable to request player counts for {1}: not connected to Steam!", details.Sender.Nickname, GetAppName( realGameID.AppID ) );
+                IRC.Instance.Send( details.Channel, "{0}: Unable to request player counts for {1}: not connected to Steam!", details.Sender.Nickname, GetAppName( appId ) );
                 return;
             }
 
-            if ( realGameID.AppID == 0 )
+            if ( appId == 0 )
             {
                 // send this request as a package info request
-                Steam.Instance.Apps.PICSGetProductInfo( null, realGameID.AppID, false, false );
+                Steam.Instance.Apps.PICSGetProductInfo( null, appId, false, false );
             }
             else
             {
                 // send off a product request as well so we get something to cache for later
-                Steam.Instance.Apps.PICSGetProductInfo( realGameID.AppID, null, false, false );
+                Steam.Instance.Apps.PICSGetProductInfo( appId, null, false, false );
             }
 
-            var jobId = Steam.Instance.UserStats.GetNumberOfCurrentPlayers( gameId );
-            AddRequest( details, new Request { JobID = jobId, GameID = gameId } );
+            var jobId = Steam.Instance.UserStats.GetNumberOfCurrentPlayers( appId );
+            AddRequest( details, new Request { JobID = jobId, AppID = appId } );
         }
 
 
@@ -84,11 +79,11 @@ namespace SteamIrcBot
 
             if ( callback.Result != EResult.OK )
             {
-                IRC.Instance.Send( req.Channel, "{0}: Unable to request player counts for {1}: {2}", req.Requester.Nickname, GetAppName( req.GameID.AppID ), callback.Result );
+                IRC.Instance.Send( req.Channel, "{0}: Unable to request player counts for {1}: {2}", req.Requester.Nickname, GetAppName( req.AppID ), callback.Result );
                 return;
             }
 
-            IRC.Instance.Send( req.Channel, "{0}: {1} players: {2}", req.Requester.Nickname, GetAppName( req.GameID.AppID ), callback.NumPlayers );
+            IRC.Instance.Send( req.Channel, "{0}: {1} players: {2}", req.Requester.Nickname, GetAppName( req.AppID ), callback.NumPlayers );
         }
 
 
